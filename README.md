78bd2e2d84e8a5bddfc8760ba984fac7c33a3a78
RafaelSotero-dev <rafaeljesusabc2@gmail.com>
2023-02-09 22:21:31 -0300

    Finalizando o projeto da calculadora

---
+++ README.md
@@ -0,0 +1,4 @@
+##Projeto feito para o #05 desafio do #boraCodar da ##rocketseat##
+
+#Gif mostando a calculadora
+<img src="screenshots\CalculadoraGif.gif" alt="Mostando as funções da calculadora em um gif"/>
---
+++ index.js
@@ -0,0 +1,104 @@
+buttonsArray = [
+  '(',
+  ')',
+  '%',
+  'AC',
+  7,
+  8,
+  9,
+  '/',
+  4,
+  5,
+  6,
+  'x',
+  1,
+  2,
+  3,
+  '-',
+  0,
+  '.',
+  '=',
+  '+',
+]
+
+const createButtons = () => {
+  let count = 0
+  const containerButtons = document.getElementById('ContainerButtons')
+  while (count < 20) {
+    createElementButton = document.createElement('div')
+
+    containerButtons.appendChild(createElementButton)
+
+    count++
+  }
+}
+
+const colorButtons = (button) => {
+  if (
+    Number.isInteger(Number(button.textContent)) === false ||
+    Number(button.textContent) === 0
+  ) {
+    return button.style.backgroundColor = '#3E2968'
+  }
+}
+
+const cleanInput = (button) => {
+  if (button.textContent === 'AC') {
+    document.getElementById('InputDisplay').value = ''
+  }
+}
+
+const equalFunction = (button) => {
+  if (button.textContent === '=') {
+    document.getElementById('LastCalc').innerHTML =
+      document.getElementById('InputDisplay').value
+
+    const result = document.getElementById('InputDisplay').value
+    document.getElementById('InputDisplay').value = eval(result)
+  }
+}
+
+const showNumbersInInput = (event, button) => {
+  const inputElemet = document.getElementById('InputDisplay').value
+
+  if (
+    button.textContent != 'AC' &&
+    button.textContent != '='
+  ) {
+    if (button.textContent === 'x') {
+      event.target.textContent = '*'
+      document.getElementById('InputDisplay').value =
+        inputElemet + event.target.textContent
+    }
+    document.getElementById('InputDisplay').value =
+      inputElemet + event.target.textContent
+  }
+}
+
+const insertInButtons = (buttons) => {
+  containerButtons = document.querySelectorAll('#ContainerButtons > div')
+
+  for (let btn = 0; btn < buttons.length; btn++) {
+    containerButtons[btn].classList.add('btn')
+
+    contentButton = document.createTextNode(buttons[btn])
+    containerButtons[btn].appendChild(contentButton)
+
+    colorButtons(containerButtons[btn])
+
+    containerButtons[btn].addEventListener('click', (e) => {
+      cleanInput(containerButtons[btn])
+
+      showNumbersInInput(e, containerButtons[btn])
+
+      if (document.getElementById('InputDisplay').value.length > 0) {
+        equalFunction(containerButtons[btn])
+      }
+    })
+  }
+}
+
+window.onload = () => {
+  createButtons()
+  insertInButtons(buttonsArray)
+}
